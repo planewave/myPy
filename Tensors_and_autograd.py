c5c303aecfabf992d3731da8c19458f23f0cb5b2
liuxiao <liuxiao216@gmail.com>
2018-05-08 13:35:16 -0300

    back to normal

--- Tensors_and_autograd.py
+++ Tensors_and_autograd.py
@@ -7,30 +7,29 @@ device = torch.device("cpu")
 
 # N is batch size; D_in is input dimension;
 # H is hidden dimension; D_out is output dimension.
-N, D_in, H, D_out = 64, 10, 100, 10
+N, D_in, H, D_out = 64, 20, 100, 10
 
 # Create random Tensors to hold input and outputs.
 # Setting requires_grad=False indicates that we do not need to compute gradients
 # with respect to these Tensors during the backward pass.
 x = torch.randn(N, D_in, device=device, dtype=dtype)
-# y = torch.randn(N, D_out, device=device, dtype=dtype)
-y = x+15;
+y = torch.randn(N, D_out, device=device, dtype=dtype)
+
 
 # Create random Tensors for weights.
 # Setting requires_grad=True indicates that we want to compute gradients with
 # respect to these Tensors during the backward pass.
-w1 = torch.randn(N, D_in, device=device, dtype=dtype, requires_grad=True)
-w2 = torch.randn(D_in, D_in, device=device, dtype=dtype, requires_grad=True)
+w1 = torch.randn(D_in, H, device=device, dtype=dtype, requires_grad=True)
+w2 = torch.randn(H, D_out, device=device, dtype=dtype, requires_grad=True)
 # w2 = torch.randn(H, D_out, device=device, dtype=dtype, requires_grad=True)
 
-learning_rate = 1e-2
+learning_rate = 1e-4
 for t in range(500):
     # Forward pass: compute predicted y using operations on Tensors; these
     # are exactly the same operations we used to compute the forward pass using
     # Tensors, but we do not need to keep references to intermediate values since
     # we are not implementing the backward pass by hand.
-    # y_pred = x.mm(w1).clamp(min=0).mm(w2)
-    y_pred = (x+w1)
+    y_pred = x.mm(w1).clamp(min=0).mm(w2)
 
     # Compute and print loss using operations on Tensors.
     # Now loss is a Tensor of shape (1,)
